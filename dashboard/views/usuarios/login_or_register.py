5a885dcd959453b6f957c7b561e8c047a564937d
GustavOxys <gustavohzmdev@gmail.com>
2024-04-19 13:07:05 -0300

    Responsividade na tela de login e registro, view modificada

--- dashboard/views/usuarios/login_or_register.py
+++ dashboard/views/usuarios/login_or_register.py
@@ -1,32 +1,51 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from dashboard.forms.usuarios.form_user import RegisterForm
 from dashboard.forms.usuarios.form_custom import CustomAuthenticationForm
+from icecream import ic
 
 def login_or_register(request):
     if request.method == 'POST':
-        if 'login_form' in request.POST:
+        ic()
+        ic(request.POST)
+        form_type = request.POST.get('form_type')
+        ic(form_type)
+        if form_type == 'login':
+            ic()
             form = CustomAuthenticationForm(request, data=request.POST)
             if form.is_valid():
+                ic()
                 user = form.get_user()
                 login(request, user)
-                return JsonResponse({'success': True, 'redirect_url': '/dashboard/'})
+                return redirect('dashboard:index')
             else:
+                ic()
                 return JsonResponse({'success': False, 'errors': form.errors})
-        elif 'register_form' in request.POST:
+        if form_type == 'register':
+            ic()
             form = RegisterForm(request.POST)
             if form.is_valid():
+                ic()
                 user = form.save()
-                login(request, user)
-                return JsonResponse({'success': True, 'redirect_url': '/dashboard/'})
+                
+                messages.success(request, 'Usuário registrado com sucesso!') 
+                
+                return redirect('dashboard:login_or_register') 
             else:
-                return JsonResponse({'success': False, 'errors': form.errors})
+                ic()
+                messages.error(request, 'Ocorreu algum erro ao enviar o formulário') 
+                return redirect('dashboard:login_or_register') 
+        else:
+            ic()
     else:
-        login_form = CustomAuthenticationForm()
+        ic()
+        login_form = CustomAuthenticationForm(request)
         register_form = RegisterForm()
+        ic(login_form)
+        ic(register_form)
         
         context = {
             'login_form' : login_form,
